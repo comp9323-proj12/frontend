46f7e014b1dcf1ba5406fdf6b1efd7b4ad815dbe
QIanhuiGuo <86042961+QIanhuiGuo@users.noreply.github.com>
2021-08-08 23:24:15 +1000

    new_login_page_with_bug

--- src/pages/User/Login.jsx
+++ src/pages/User/Login.jsx
@@ -78,264 +78,297 @@ const Login = ({ dispatch, registerFlag, reversalRegistered }) => {
     setIsRegisterModalVisible(false);
   };
   return (
-    <Row>
-      <Col className={styles['login__banner']} span={8}>
-        <Carousel autoplay autoplaySpeed={3000}>
-          <img src={require('@/images/login-banner_1.png')} alt="banner-1" />
-          <img src={require('@/images/login-banner_2.png')} alt="banner-2" />
-        </Carousel>
-      </Col>
-      <Col className={styles['login__main']} span={16}>
-        <Form
-          name="login"
-          className={styles['login__form']}
-          wrapperCol={{
-            span: 18,
-          }}
-          onFinish={onLoginFinish}
-        >
-          <Form.Item
-            name="email"
-            rules={[
-              {
-                type: 'email',
-                message: 'The input is not valid E-mail!',
-              },
-              {
-                required: true,
-                message: 'Please input your E-mail!',
-              },
-            ]}
-          >
-            <Input placeholder="Email" />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your password!',
-              },
-            ]}
-          >
-            <Input.Password placeholder="Password" />
-          </Form.Item>
-          <Form.Item
-            wrapperCol={{
-              offset: 8,
-              span: 16,
-            }}
-          >
-            <Button
-              className={styles['login__button']}
-              type="primary"
-              htmlType="submit"
-            >
-              Login
-            </Button>
-          </Form.Item>
-          <Form.Item
-            wrapperCol={{
-              offset: 8,
-              span: 16,
-            }}
-          >
-            <Button
-              className={styles['login__button']}
-              onClick={renderRegisterModal}
-            >
-              Register
-            </Button>
-          </Form.Item>
-        </Form>
-      </Col>
-      {/* <Modal
-        title="Login Failed"
-        footer={null}
-        visible={isLoginModalVisible}
-        onCancel={handleCancel}
-      >
-        <p>Login failed, please check your username and password!</p>
-      </Modal> */}
-      <Modal
-        title="Register"
-        footer={null}
-        destroyOnClose={true}
-        visible={isRegisterModalVisible}
-        onCancel={handleCancel}
-      >
-        <Form
-          preserve={false}
-          form={form}
-          name="register"
-          className={styles['register__form']}
-          onFinish={onRegisterFinish}
-        >
-          <Form.Item
-            name="name"
-            label="Name"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your name!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="email"
-            label="E-mail"
-            rules={[
-              {
-                type: 'email',
-                message: 'The input is not valid E-mail!',
-              },
-              {
-                required: true,
-                message: 'Please input your E-mail!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            label="Password"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your password!',
-              },
-            ]}
-            hasFeedback
-          >
-            <Input.Password />
-          </Form.Item>
-          <Form.Item
-            name="confirm"
-            label="Confirm Password"
-            dependencies={['password']}
-            hasFeedback
-            rules={[
-              {
-                required: true,
-                message: 'Please confirm your password!',
-              },
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value || getFieldValue('password') === value) {
-                    return Promise.resolve();
-                  }
-                  return Promise.reject(
-                    new Error(
-                      'The two passwords that you entered do not match!',
-                    ),
-                  );
-                },
-              }),
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-          <Form.Item name="dob" label="Date of Birth">
-            <DatePicker />
-          </Form.Item>
-          <Form.Item name="gender" label="Gender">
-            <Select>
-              <Select.Option value="Unknown">Unknown</Select.Option>
-              <Select.Option value="Female">Female</Select.Option>
-              <Select.Option value="Male">Male</Select.Option>
-            </Select>
-          </Form.Item>
-          <Form.Item name="country" label="Country">
-            <Input />
-          </Form.Item>
-          <Form.Item name="contactNumber" label="Contact Number">
-            <Input />
-          </Form.Item>
-          <Form.Item label="Research Area">
-            <Form.List name="researchArea">
-              {(fields, { add, remove }, { errors }) => (
-                <>
-                  {fields.map((field, index) => (
-                    <Form.Item required={false} key={field.key}>
-                      <Form.Item {...field} noStyle>
-                        <Input
-                          placeholder="research area"
-                          style={{ width: '60%' }}
-                        />
-                      </Form.Item>
-                      {fields.length > 1 ? (
-                        <MinusCircleOutlined
-                          className="dynamic-delete-button"
-                          onClick={() => remove(field.name)}
-                        />
-                      ) : null}
-                    </Form.Item>
-                  ))}
-                  <Form.Item>
+    <>
+      <div className={styles['login']}>
+        <Row>
+          <p className={styles['login__title']}>
+            {' '}
+            Your Research. Your Life. Your Story.{' '}
+          </p>
+        </Row>
+        <Row>
+          <p className={styles['login__word']}>
+            {' '}
+            A magnetic community of researchers bound by their stories,
+          </p>
+        </Row>
+        <Row>
+          <div className={styles['login__word']}>
+            {' '}
+            where let you share research in a way that is understandable and
+            interesting.{' '}
+          </div>
+        </Row>
+        <Row>
+          <Col className={styles['whitebg']} span={12} offset={6}>
+            <Row>
+              <Col className={styles['login__main']} span={12}>
+                <Form
+                  name="login"
+                  className={styles['login__form']}
+                  wrapperCol={{
+                    span: 18,
+                  }}
+                  onFinish={onLoginFinish}
+                >
+                  <Form.Item name="slogan">
+                    <div className={styles['login__here']}>Login Here</div>
+                  </Form.Item>
+                  <Form.Item
+                    name="email"
+                    rules={[
+                      {
+                        type: 'email',
+                        message: 'The input is not valid E-mail!',
+                      },
+                      {
+                        required: true,
+                        message: 'Please input your E-mail!',
+                      },
+                    ]}
+                  >
+                    <Input placeholder="Email" />
+                  </Form.Item>
+                  <Form.Item
+                    name="password"
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Please input your password!',
+                      },
+                    ]}
+                  >
+                    <Input.Password placeholder="Password" />
+                  </Form.Item>
+                  <Form.Item
+                    wrapperCol={{
+                      offset: 8,
+                      span: 16,
+                    }}
+                  >
                     <Button
-                      type="dashed"
-                      onClick={() => add()}
-                      style={{ width: '60%' }}
-                      icon={<PlusOutlined />}
+                      className={styles['login__button']}
+                      type="primary"
+                      htmlType="submit"
                     >
-                      Add research area
+                      Login
                     </Button>
                   </Form.Item>
-                </>
-              )}
-            </Form.List>
-          </Form.Item>
-          <Form.Item label="Publications">
-            <Form.List name="publications">
-              {(fields, { add, remove }, { errors }) => (
-                <>
-                  {fields.map((field, index) => (
-                    <Form.Item required={false} key={field.key}>
-                      <Form.Item {...field} noStyle>
-                        <Input
-                          placeholder="publications"
-                          style={{ width: '60%' }}
-                        />
-                      </Form.Item>
-                      {fields.length > 1 ? (
-                        <MinusCircleOutlined
-                          className="dynamic-delete-button"
-                          onClick={() => remove(field.name)}
-                        />
-                      ) : null}
-                    </Form.Item>
-                  ))}
-                  <Form.Item>
+                  <Form.Item
+                    wrapperCol={{
+                      offset: 8,
+                      span: 16,
+                    }}
+                  >
                     <Button
-                      type="dashed"
-                      onClick={() => add()}
-                      style={{ width: '60%' }}
-                      icon={<PlusOutlined />}
+                      className={styles['login__button']}
+                      onClick={renderRegisterModal}
                     >
-                      Add publications
+                      Register
                     </Button>
                   </Form.Item>
-                </>
-              )}
-            </Form.List>
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-          <Form.Item>
-            <Button htmlType="button" onClick={onReset}>
-              Reset
-            </Button>
-          </Form.Item>
-          {/* TODO: biography, career是啥？ */}
-        </Form>
-      </Modal>
-    </Row>
+                </Form>
+              </Col>
+              <Col span={12} offset={12}>
+                <Carousel autoplay autoplaySpeed={3000}>
+                  <img
+                    src={require('@/images/login-banner_1.png')}
+                    alt="banner-1"
+                  />
+                  <img
+                    src={require('@/images/login-banner_2.png')}
+                    alt="banner-2"
+                  />
+                </Carousel>
+              </Col>
+            </Row>
+          </Col>
+          <Modal
+            title="Register"
+            footer={null}
+            destroyOnClose={true}
+            visible={isRegisterModalVisible}
+            onCancel={handleCancel}
+          >
+            <Form
+              preserve={false}
+              form={form}
+              name="register"
+              className={styles['register__form']}
+              onFinish={onRegisterFinish}
+            >
+              <Form.Item
+                name="name"
+                label="Name"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your name!',
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                name="email"
+                label="E-mail"
+                rules={[
+                  {
+                    type: 'email',
+                    message: 'The input is not valid E-mail!',
+                  },
+                  {
+                    required: true,
+                    message: 'Please input your E-mail!',
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                label="Password"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your password!',
+                  },
+                ]}
+                hasFeedback
+              >
+                <Input.Password />
+              </Form.Item>
+              <Form.Item
+                name="confirm"
+                label="Confirm Password"
+                dependencies={['password']}
+                hasFeedback
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please confirm your password!',
+                  },
+                  ({ getFieldValue }) => ({
+                    validator(_, value) {
+                      if (!value || getFieldValue('password') === value) {
+                        return Promise.resolve();
+                      }
+                      return Promise.reject(
+                        new Error(
+                          'The two passwords that you entered do not match!',
+                        ),
+                      );
+                    },
+                  }),
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
+              <Form.Item name="dob" label="Date of Birth">
+                <DatePicker />
+              </Form.Item>
+              <Form.Item name="gender" label="Gender">
+                <Select>
+                  <Select.Option value="Unknown">Unknown</Select.Option>
+                  <Select.Option value="Female">Female</Select.Option>
+                  <Select.Option value="Male">Male</Select.Option>
+                </Select>
+              </Form.Item>
+              <Form.Item name="country" label="Country">
+                <Input />
+              </Form.Item>
+              <Form.Item name="contactNumber" label="Contact Number">
+                <Input />
+              </Form.Item>
+              <Form.Item label="Research Area">
+                <Form.List name="researchArea">
+                  {(fields, { add, remove }, { errors }) => (
+                    <>
+                      {fields.map((field, index) => (
+                        <Form.Item required={false} key={field.key}>
+                          <Form.Item {...field} noStyle>
+                            <Input
+                              placeholder="research area"
+                              style={{ width: '60%' }}
+                            />
+                          </Form.Item>
+                          {fields.length > 1 ? (
+                            <MinusCircleOutlined
+                              className="dynamic-delete-button"
+                              onClick={() => remove(field.name)}
+                            />
+                          ) : null}
+                        </Form.Item>
+                      ))}
+                      <Form.Item>
+                        <Button
+                          type="dashed"
+                          onClick={() => add()}
+                          style={{ width: '60%' }}
+                          icon={<PlusOutlined />}
+                        >
+                          Add research area
+                        </Button>
+                      </Form.Item>
+                    </>
+                  )}
+                </Form.List>
+              </Form.Item>
+              <Form.Item label="Publications">
+                <Form.List name="publications">
+                  {(fields, { add, remove }, { errors }) => (
+                    <>
+                      {fields.map((field, index) => (
+                        <Form.Item required={false} key={field.key}>
+                          <Form.Item {...field} noStyle>
+                            <Input
+                              placeholder="publications"
+                              style={{ width: '60%' }}
+                            />
+                          </Form.Item>
+                          {fields.length > 1 ? (
+                            <MinusCircleOutlined
+                              className="dynamic-delete-button"
+                              onClick={() => remove(field.name)}
+                            />
+                          ) : null}
+                        </Form.Item>
+                      ))}
+                      <Form.Item>
+                        <Button
+                          type="dashed"
+                          onClick={() => add()}
+                          style={{ width: '60%' }}
+                          icon={<PlusOutlined />}
+                        >
+                          Add publications
+                        </Button>
+                      </Form.Item>
+                    </>
+                  )}
+                </Form.List>
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+              <Form.Item>
+                <Button htmlType="button" onClick={onReset}>
+                  Reset
+                </Button>
+              </Form.Item>
+            </Form>
+          </Modal>
+        </Row>
+        <Row>
+          <div className={styles['login__copyright']}>
+            {' '}
+            ©2021 Research Story. All rights reserved{' '}
+          </div>
+        </Row>
+      </div>
+    </>
   );
 };
 
